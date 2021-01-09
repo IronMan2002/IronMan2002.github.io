b2a450a7f61b6859c55e52f07409a9e1bd6ede7f
IronMan2002 <2586471484@qq.com>
2021-01-09 18:25:02 +0800

    Site updated: 2021-01-09 18:25:00

---
+++ dist/music.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    fixed: true,
+    audio: [{
+        name: 'name',
+        artist: 'artist',
+        url: 'url.mp3',
+        cover: 'cover.jpg',
+    }]
+});
